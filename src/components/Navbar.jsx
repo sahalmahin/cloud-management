9ff97162a1d887a9ace1a43b0f91f82b0a373810
sahalmahin <samirsatter0142@gmail.com>
2024-02-21 04:19:04 +0600

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import { MdMenu } from "react-icons/md";
 import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
+
     return (
         <div className="w-screen h-[80px] bg-zinc-200 drop-shadow-lg fixed">
             <div className="px-2 flex justify-between items-center w-full h-full">
@@ -16,23 +21,25 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="hidden md:flex pr-4">
-                    <button className="border-0 bg-transparent text-black mr-4">Sign In</button>
+                    <button className="border-none bg-transparent text-black mr-4">Sign In</button>
                     <button className="px-5 py-3">Sign Up</button>
                 </div>
-                <div className="md:hidden">
-                    <MdMenu className="w-8" />
+                <div className="md:hidden" onClick={handleClick}>
+                    {!nav ? <MdMenu className="w-8" /> : <AiOutlineClose className="w-8" />}
                 </div>
             </div>
 
-            <ul className="absolute bg-zinc-200 w-full px-8">
-                        <li className="border-b-2 border-zinc-300 w-full">Home</li>
-                        <li className="border-b-2 border-zinc-300 w-full">About</li>
-                        <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
-                        <li className="border-b-2 border-zinc-300 w-full">Platform</li>
-                        <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
-                    </ul>
-
-            <AiOutlineClose />
+            <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
+                <li className="border-b-2 border-zinc-300 w-full">Home</li>
+                <li className="border-b-2 border-zinc-300 w-full">About</li>
+                <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+                <li className="border-b-2 border-zinc-300 w-full">Platform</li>
+                <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+                <div className="flex flex-col my-4">
+                    <button className="bg-transparent text-indigo-600 px-5 py-3 mb-4">Sign In</button>
+                    <button className="px-5 py-3">Sign Up</button>
+                </div>
+            </ul>
         </div>
     );
 };
