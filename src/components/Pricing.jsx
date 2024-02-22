b0544bf03c9b9d0008b9dd390d0aff8b9a3c2e43
sahalmahin <samirsatter0142@gmail.com>
2024-02-23 02:57:57 +0600

    pricing has been done

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,26 +1,51 @@
+import { FaCheckDouble } from "react-icons/fa6";
+
 const Pricing = () => {
     return (
-        <div>
-            <div>
-                Absolute
-            </div>
+        <div name='pricing' className='w-full text-white my-24 bg-slate-900 absolute'>
 
-            <div>
-                <div>
-                    <h2>Pricing</h2>
-                    <h3>The right price for your research.</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus voluptatum minima, placeat optio voluptatibus? Dolorem doloribus iste hic adipisci!</p>
+            <div className='max-w-[1240px] mx-auto py-12'>
+
+                <div className='text-center py-8 text-slate-300'>
+                    <h2 className='text-3xl uppercase'>Pricing</h2>
+                    <h3 className='text-5xl font-bold py-8'>The right price for your research.</h3>
+                    <p className='text-3xl'>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+                        laudantium odio ullam inventore aliquid ipsum quasi tenetur velit
+                        voluptatum iste.
+                    </p>
                 </div>
 
-                <div>
-                    <div>
-                        <span>Standard</span>
+                <div className='grid md:grid-cols-2'>
+
+                    <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
+                        <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Standard</span>
                         <div>
-                            <p>$49<span>/mon</span></p>
+                            <p className='text-6xl font-bold py-4 flex'>$49<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, soluta?</p>
+                        <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <div className='text-2xl'>
+                            <p className='flex py-4'><FaCheckDouble className='mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                            <p className='flex py-4'><FaCheckDouble className='mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                            <p className='flex py-4'><FaCheckDouble className='mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                            <p className='flex py-4'><FaCheckDouble className='mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                            <p className='flex py-4'><FaCheckDouble className='mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                            <button className='w-full py-4 my-4'>Get Started</button>
+                        </div>
+                    </div>
+                    <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
+                        <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Premium</span>
                         <div>
-                            
+                            <p className='text-6xl font-bold py-4 flex'>$99<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
+                        </div>
+                        <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <div className='text-2xl'>
+                            <p className='flex py-4'><FaCheckDouble className='mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                            <p className='flex py-4'><FaCheckDouble className='mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                            <p className='flex py-4'><FaCheckDouble className='mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                            <p className='flex py-4'><FaCheckDouble className='mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                            <p className='flex py-4'><FaCheckDouble className='mr-5 text-green-600' />Lorem, ipsum dolor.</p>
+                            <button className='w-full py-4 my-4'>Get Started</button>
                         </div>
                     </div>
                 </div>
