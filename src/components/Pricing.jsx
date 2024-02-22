a815f46e80a8fb03118736f9b9ef350a49e610d7
sahalmahin <samirsatter0142@gmail.com>
2024-02-23 02:13:32 +0600

    initialized-pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,7 +1,30 @@
 const Pricing = () => {
     return (
         <div>
-            
+            <div>
+                Absolute
+            </div>
+
+            <div>
+                <div>
+                    <h2>Pricing</h2>
+                    <h3>The right price for your research.</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus voluptatum minima, placeat optio voluptatibus? Dolorem doloribus iste hic adipisci!</p>
+                </div>
+
+                <div>
+                    <div>
+                        <span>Standard</span>
+                        <div>
+                            <p>$49<span>/mon</span></p>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, soluta?</p>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
